df666a4b6935755e513b15b942570cf28831885f
GiaNik0la <gianikolishvili0804@gmail.com>
2023-09-30 09:15:32 +0400

    F**K YEA

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 #include "include/QuadTree.h"
 #include "include/Point.h"
 #include "include/raylib.h"
@@ -12,13 +13,19 @@ int main(void) {
   InitWindow(width, height, "QuadTree");
 
   struct QuadTree *qtree = createNode(0, 0, width, height, 4);
-
+/*
   for (int i = 0; i < 50; i++) {
     struct Point p = {i, i};
     insertPoint(qtree, p);
   }
+*/
 
   while (!WindowShouldClose()) {
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+      struct Point p = {round(GetMousePosition().x), round(GetMousePosition().y)};
+      insertPoint(qtree, p);
+    }
+
     BeginDrawing();
       ClearBackground(BLACK);
       drawTree(qtree);
